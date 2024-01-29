651ffacd6540ab7d6487190fbb0a8c971b1001ec
Aayush Upreti <136055230+ayus-10@users.noreply.github.com>
2024-01-29 17:29:39 +0545

    write logic for route /quiz

--- routes/quiz.js
+++ routes/quiz.js
@@ -3,18 +3,28 @@ const express = require("express");
 
 const router = express.Router();
 
-router.post("/", async (req, res) => {
+router.use(async (req, res, next) => {
   const credentials = req.body;
   const quizData = await quiz.findOne({ id: credentials.id });
   if (quizData) {
     if (quizData.password === credentials.password) {
-      res.status(200).json(quizData.questions);
+      req.credentials = credentials;
+      req.questions = quizData.questions;
+      next();
     } else {
       res.status(400).send("Invalid Password");
     }
   } else {
     res.status(400).send("Invalid ID");
   }
+});
+
+router.post("/credentials", async (req, res) => {
+  res.status(200).json(req.credentials);
+});
+
+router.post("/questions", async (req, res) => {
+  res.status(200).json(req.questions);
 });
 
 module.exports = router;

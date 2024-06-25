edd64804813c1102ee545a554c5dc695fb203b5a
ayus-10 <136055230+ayus-10@users.noreply.github.com>
2024-06-25 12:44:15 +0545

    Installed package cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import usersRoute from "./routes/users.route";
 
 dotenv.config();
@@ -9,6 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 const DB_URI = process.env.DB_URI as string;
 const PORT = process.env.PORT || 8080;
